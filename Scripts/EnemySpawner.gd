94b78e637c3cc0d6418838b9022af5b9c0d13eb2
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2022-10-15 17:19:05 -0300

    spawn only outscreen

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -1,34 +1,63 @@
 extends Node2D
 
 
-var enemy = load("res://Objects/EnemyPath.tscn")
+onready var enemy = load("res://Objects/EnemyPath.tscn")
 onready var areaSize = $Area2D/CollisionShape2D.get_shape().get_extents()
 onready var timer = $SpawnTimer
 onready var enemyArea = $EnemyArea
 var timerDefaultWaitTime
-var canSpawn = false
-export var maxEnemies
+var canSpawn = true
+export var maxEnemies = 5
+export var spawnFrequency = 5.0
 onready var enemies = $Enemies
+export var spawnPositions = []
+var enemyInPosition = []
+onready var visibilityNotifier = $VisibilityNotifier2D
 
 func _ready():
+	for i in $EnemyAreas.get_children():
+		spawnPositions.append(i)
+	timer.wait_time = spawnFrequency
+	spawnPositions.shuffle()
+	for i in range (spawnPositions.size()):
+		enemyInPosition.append(null)
+#		SpawnEnemy(i)
 	timer.start()
 
 func _on_SpawnTimer_timeout():
-#	timerDefaultWaitTime = rand_range(5,10)
-	set_spawn_position()
-	yield(get_tree().create_timer(0.2), "timeout")
-	if canSpawn:
-		var enemy_instance = enemy.instance()
-		enemies.add_child(enemy_instance)
-		enemy_instance.global_position = enemyArea.global_position
+	timer.start()
+	if enemies.get_child_count() >= maxEnemies:
 		pass
-		
+	else:
+		if canSpawn:
+			set_spawn_position()
+#		yield(get_tree().create_timer(0.2), "timeout")
+#		if canSpawn:
+#			SpawnEnemy()
+
+func SpawnEnemy(pos):
+	var enemy_instance = enemy.instance()
+	enemies.add_child(enemy_instance)
+	enemy_instance.global_position = pos.global_position
+	pass
 
 func set_spawn_position():
-	enemyArea.global_position = to_global(Vector2(rand_range(-areaSize.x, areaSize.x), rand_range(-areaSize.y, areaSize.y)))
-	yield(get_tree().create_timer(0.1), "timeout")
-	if enemyArea.get_overlapping_bodies() != []:
-		set_spawn_position()
-		canSpawn = false
-	else:
-		canSpawn = true
+	enemyInPosition.shuffle()
+	for i in range(enemyInPosition.size()):
+		if enemyInPosition[i] == null:
+			SpawnEnemy(spawnPositions[i])
+#	enemyArea.global_position = to_global(Vector2(rand_range(-areaSize.x, areaSize.x), rand_range(-areaSize.y, areaSize.y)))
+#	yield(get_tree().create_timer(0.01), "timeout")
+#	if enemyArea.get_overlapping_bodies() != []:
+#		set_spawn_position()
+#		canSpawn = false
+#	else:
+#		canSpawn = true
+
+func _on_VisibilityNotifier2D_screen_entered():
+	canSpawn = false
+	pass # Replace with function body.
+
+func _on_VisibilityNotifier2D_screen_exited():
+	canSpawn = true
+	pass # Replace with function body.
