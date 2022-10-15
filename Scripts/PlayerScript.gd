d0350a5e41a6be439ee477feef4b601a305daac3
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2022-10-15 04:31:12 -0300

    player improvements

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -12,14 +12,18 @@ var canMove = true
 var canPlay = true
 var inputEnabled = true
 var moveDir = Vector2(-1,0)
-var friction = true
+var noMovementInput = true
 var walking = false
 var acceleration = 75
 var maxSpeed = 150
 var canJump = true
 var justJumped = false
-var jumpHeight = -500
-var airAcceleration = 50
+var jumpHeight = 500
+#var airAcceleration = 50
+var currentCoyote = 0
+var maxCoyote = 0.2
+var jumpDirection = Vector2(0,-1)
+var wallDirection = 0
 onready var sprite = $Sprite
 onready var animPlayer = $AnimationPlayer
 onready var wallraycast = $WallRaycast
@@ -27,67 +31,100 @@ onready var raycastSide = $WallRaycast/RayCastSides
 onready var raycastSide2 = $WallRaycast/RayCastSides2
 onready var raycastUp = $RayCastUp
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	#motion.y = min(motion.y+gravity, maxGravity)
-	if !canMove:
-		if is_on_floor():
-			animPlayer.play("Idle")
-		else:
-			animPlayer.play("Fall")
-	
-	elif Input.is_action_pressed("ui_right") and inputEnabled:
+	if !is_on_floor():
+		currentCoyote += _delta
+		if currentCoyote > maxCoyote:
+			canJump = false
+		
+	moveDir = Vector2(0,0)
+#	if !canMove:
+#		if is_on_floor():
+#			animPlayer.play("Idle")
+#		else:
+#			animPlayer.play("Fall")
+	if Input.is_action_pressed("ui_right") and inputEnabled:
 		moveDir.x = 1
 		move()
 	elif Input.is_action_pressed("ui_left") and inputEnabled:
 		moveDir.x = -1
 		move()
-	elif Input.is_action_pressed("ui_up") and _check_is_valid_wall() and inputEnabled:
+	if Input.is_action_pressed("ui_up") and _check_is_valid_wall() and inputEnabled:
 		moveDir.y = -1
-		moveDir.x = 0
+#		moveDir.x = 0
 		move()
 	elif Input.is_action_pressed("ui_down") and _check_is_valid_wall() and inputEnabled:
 		moveDir.y = 1
-		moveDir.x = 0
+#		moveDir.x = 0
 		move()
-	else:
-		animPlayer.play("Idle")
-		friction = true
+		
+	if moveDir == Vector2(0,0):
+		if is_on_floor():
+			animPlayer.play("Idle")
+		else:
+			animPlayer.play("Fall")
+			
+		noMovementInput = true
 		walking = false
+#		elif _check_is_valid_wall():
+#			animPlayer.play("IdleWall")
 	
-	if Input.is_action_pressed("Jump") and inputEnabled:
-		Jump()
 	
 	if _check_is_valid_wall() or raycastUp.is_colliding():
 		canJump = true
-		if !walking:
-			motion.y = 0
-		#motion.y += 0
+		currentCoyote = 0
+		if moveDir.y == -1:
+			if motion.y > 0:
+				motion.y = 0
+		elif moveDir.y == 1:
+			if motion.y < 0:
+				motion.y = 0
+		elif moveDir.x != 0:
+			if motion.y < 0:
+				motion.y = 0
+			if motion.y > 0:
+				motion.y = 0
+		else:
+			if motion.y > 0:
+				motion.y -= gravity
+			elif motion.y < 0:
+				motion.y += gravity
 	else:
 		motion.y += gravity
 		sprite.flip_v = false
+		
+	if Input.is_action_just_pressed("Jump") and inputEnabled:
+		Jump()
 	
 	if is_on_floor() or raycastUp.is_colliding():
 		canJump = true
+		currentCoyote = 0
 		sprite.flip_v = false
-		if (friction):
+		if (moveDir.x == 0):
 			motion.x = lerp(motion.x, 0, 0.2)
+		if raycastUp.is_colliding():
+			jumpDirection.y = 0.5
+	else:
+		jumpDirection.y = -1
+		
 	
-	if _check_is_valid_wall() and friction:
+	if _check_is_valid_wall() and noMovementInput:
 		motion.y = lerp(motion.y, 0, 0.2)
 	motion = move_and_slide(motion, UP)
 
 func move():
 #	if invertControls:
 #		move_dir *= -1
-	if moveDir.x > 0:
+	if moveDir.x > 0 and motion.x < maxSpeed:
 		motion.x = min(motion.x+acceleration, maxSpeed)
 		sprite.flip_h = true
 		animPlayer.play("Run")
-	elif moveDir.x < 0:
+	elif moveDir.x < 0 and motion.x > -maxSpeed:
 		motion.x = max(motion.x-acceleration, -maxSpeed)
 		sprite.flip_h = false
 		animPlayer.play("Run")
-	elif moveDir.y > 0:
+	if moveDir.y > 0:
 		motion.y = min(motion.y+acceleration, maxSpeed)
 		animPlayer.play("Climb")
 		#rodar o player na parede? 90º
@@ -97,17 +134,24 @@ func move():
 		sprite.flip_v = false
 		animPlayer.play("Climb")
 		# rodar -90º?
-	friction = false
+	noMovementInput = false
 	walking = true
 
 func Jump():
 	if canJump:
-		motion.y = jumpHeight 
-		if _check_is_valid_wall() && walking:
-			motion.x = maxSpeed*15*(moveDir.x)
+		if _check_is_valid_wall():
+			print(wallDirection)
+			jumpDirection.x = wallDirection
+			motion.x = maxSpeed*3*(jumpDirection.x)
+		else:
+			jumpDirection.x = moveDir.x * 2
+			motion.x = maxSpeed*(jumpDirection.x)
+			print(jumpDirection.x)
+			
+		motion.y = jumpDirection.y * jumpHeight 
 		#$JumpSound.play()
 		canJump = false
-		acceleration = airAcceleration
+#		acceleration = airAcceleration
 #		justJumped = true
 #		yield(get_tree().create_timer(maxCoyoteTime), "timeout") 
 #		justJumped = false
@@ -115,7 +159,12 @@ func Jump():
 func _check_is_valid_wall():
 	for raycasts in wallraycast.get_children():
 		if raycasts.is_colliding():
+			if raycasts.get_collision_point().x > global_position.x:
+				wallDirection = -1
+			elif raycasts.get_collision_point().x < global_position.x:
+				wallDirection = 1
 			return true
+	wallDirection = 0
 	return false
 
 #func play_animation(animation):
