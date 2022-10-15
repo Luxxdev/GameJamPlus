ba3a0ba8f45d9cc37028ab853ede57421e110608
gigiofig <gfps2011@gmail.com>
2022-10-15 19:10:58 -0300

    adjusted Playeranims and fixed shooting enemy

--- ShootingEnemy.gd
+++ ShootingEnemy.gd
@@ -1,21 +1,21 @@
 extends Sprite
 class_name Enemy
 
-var angleConeOfVision = deg2rad(30.0)
-var maxViewDist = 800.0
-var angleBetweenRays = deg2rad(5.0)
+#var angleConeOfVision = deg2rad(30.0)
+#var maxViewDist = 800.0
+#var angleBetweenRays = deg2rad(5.0)
 var PlayerInArea = false
 var target = null
+onready var ray = $RayCast2D
 
 
 func _process(delta):
 	if PlayerInArea:
 		look_at(target.global_position)
-	if $RayCast2D.is_colliding():
-		if $RayCast2D.get_collider() is Player:
-			print('aaaa')
-		if $ShootDelay.is_stopped():
-			Shoot()
+	if ray.is_colliding():
+		if ray.get_collider() is Player:
+			if $ShootDelay.is_stopped():
+				Shoot()
 
 
 func _on_Area2D_body_entered(body):
@@ -32,9 +32,9 @@ func Shoot():
 	print("tiro")
 	$ShootDelay.start()
 
-func GenerateRaycasts():
-	var rayCount = angleConeOfVision / angleBetweenRays
-	for i in rayCount:
-		var ray = RayCast2D.new()
-		var angle = angleBetweenRays * (i - rayCount / 2.0)
-		ray.cast_to = Vector2.UP.rotated(angle) * maxViewDist
+#func GenerateRaycasts():
+#	var rayCount = angleConeOfVision / angleBetweenRays
+#	for i in rayCount:
+#		var ray = RayCast2D.new()
+#		var angle = angleBetweenRays * (i - rayCount / 2.0)
+#		ray.cast_to = Vector2.UP.rotated(angle) * maxViewDist
